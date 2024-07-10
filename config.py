cb3edd35894ce381470350632ee7a57c3d83be0d
Daniel <danieelpark@gmail.com>
2024-07-10 15:39:19 +0300

    Переделка курсовой работы

--- config.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-HH_URL = "https://api.hh.ru/vacancies"
-
-FORMAT = '%(asctime)s %(message)s'
-
-logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="a", format=FORMAT)
--- src/vacancy.py
+++ src/vacancy.py
@@ -1,71 +1,38 @@
-class Vacancy:
-    __slots__ = ["__name", "__salary_from", "__salary_to", "__pk"]
+class PerformanceVacancies():
+    """Класс для представления вакансии"""
 
-    def __init__(self, pk: int, name: str, salary_from: int, salary_to: int) -> None:
-        self.__name = name
-        self.__pk = pk
-        self.__salary_from = self.__validate_salary(salary_from)
-        self.__salary_to = self.__validate_salary(salary_to)
-
-    @property
-    def pk(self):
-        return self.__pk
+    #
+    def __init__(self, data_vacancy: dict) -> None:
+        self.data_vacancy = data_vacancy
+        self.vacancy_id = self.data_vacancy.get('id')
+        self.vacancy_name = self.data_vacancy.get('name')
+        self.date_publishedt = self.data_vacancy.get('date_publishedt')
+        self.salary_from = self.data_vacancy.get('salary_from')
+        self.salary_to = self.data_vacancy.get('salary_to')
+        self.salary_currency = "руб."
+        self.responsibility = self.data_vacancy.get('responsibility')
+        self.town = self.data_vacancy.get('town')
+        self.url = self.data_vacancy.get('url')
 
     def __str__(self) -> str:
-        return f"Вакансия: {self.__name}, Зарплата: {self.__salary_from} - {self.__salary_to}"
+        """Строковое представление вакансии"""
+        return (f'''
+        id: {self.vacancy_id}
+        Вакансия: {self.vacancy_name}
+        Город: {self.town}
+        Дата публикации: {self.date_publishedt}
+        Заработная плата от: {self.salary_from} до: {self.salary_to} {self.salary_currency}
+        Обязанности: {self.responsibility}
+        Ссылка на вакансию: {self.url}
+        ''')
 
-    def __repr__(self) -> str:
-        return f"Вакансия: {self.__name}, Зарплата: {self.__salary_from} - {self.__salary_to}"
 
-    @staticmethod
-    def __validate_salary(salary: int) -> int:
-        return 0 if salary < 0 else salary
+class SortVacancies():
+    """Класс для сортировки списка вакансий по заданному параметру"""
 
-    def __lt__(self, other):
-        return self.__salary_from < other.__salary_from
-
-    def __gt__(self, other):
-        return self.__salary_from > other.__salary_from
-
-    @classmethod
-    def create_vacancy(cls, vacancy_data):
-        if isinstance(vacancy_data, str):
-            vacancy_data = cls.parse_string_to_dict(vacancy_data)
-        return cls(
-            pk=int(vacancy_data["pk"]),
-            name=vacancy_data["name"],
-            salary_from=int(vacancy_data["salary_from"]),
-            salary_to=int(vacancy_data["salary_to"]),
-        )
-
-    @staticmethod
-    def parse_string_to_dict(data: str) -> dict:
-        result = {}
-        items = data.split(',')
-        for item in items:
-            if '=' in item:
-                key, value = item.split('=')
-                key = key.strip()
-                value = value.strip()
-                if key in {"salary_from", "salary_to", "pk"}:
-                    try:
-                        result[key] = int(value)
-                    except ValueError:
-                        raise ValueError(f"Некорректное значение для {key}: {value}")
-                else:
-                    result[key] = value
-            else:
-                raise ValueError(f"Неверный формат для элемента: {item}")
-        required_fields = {"pk", "name", "salary_from", "salary_to"}
-        if not required_fields.issubset(result):
-            missing = required_fields - result.keys()
-            raise ValueError(f"Отсутствуют необходимые поля: {', '.join(missing)}")
-        return result
-
-    def to_dict(self) -> dict:
-        return {
-            "pk": self.__pk,
-            "salary_from": self.__salary_from,
-            "salary_to": self.__salary_to,
-        }
-
+    def sort_vacancies(self, user_param, all_vacancy, reverse=True):
+        """Принимает список вакансий, сортирует его по заданному параметру, возвращет измененный список"""
+        self.all_vacancy = all_vacancy
+        self.user_param = user_param
+        operations_sorted = sorted(self.all_vacancy, key=lambda vacancy: vacancy[self.user_param], reverse=reverse)
+        return operations_sorted
